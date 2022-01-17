8cd8cd93a6cd8a58dfdfbddcdb81353b4fbcf7dc
cuchakma <greendaycu20@gmail.com>
2022-01-17 21:32:04 +0600

    intialized socket for communication between all users

--- public/main.js
+++ public/main.js
@@ -3,6 +3,24 @@ let userName;
 let msg;
 let completeMsg;
 let textbox = document.getElementById('text-box');
+let socket = io();
+
+socket.on('message', message => {
+    console.log(message);
+});
+
+/**
+ * Chat Form(Box)
+ */
+const form = document.getElementById('chat-form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const msg = event.target.elements[0].value;
+    /**
+     * Emitting A Message To Sever
+     */
+    socket.emit('chatMessage', msg);
+});
 
 document.addEventListener('DOMContentLoaded', function(e){
     userName = prompt('What is your name?');
--- server.js
+++ server.js
@@ -1,12 +1,51 @@
-const path    = require('path');    
-const express = require('express');
+const path     = require('path');    
+const express  = require('express');
+const http     = require('http');
+const socketio = require('socket.io');
+
 const app     = express();
+const server  = http.createServer(app); 
+const io      = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'assets/js')));
 
+
+/**
+ * This Code Runs When A Client Connects(Open New Connection)
+ */
+io.on('connection', socket => {
+
+    /**
+     * This method is for sending a welcome message to the new user
+     */
+    socket.emit('message', 'Welcome To Chat Cord !');
+
+    /**
+     * This code portion is broadcasted when a user sends a message to another user
+     */
+    socket.broadcast.emit('message', 'A User Has Joined');
+
+    /**
+     * Runs When A Client Disconnects
+     */
+    socket.on('disconnect', () => {
+        /**
+         * This method runs when a client disconnects
+         */
+        io.emit('message', 'A user has left the chat');
+    });
+
+    /**
+     * Listen for chatMessage
+     */
+    socket.on('chatMessage', msg => {
+        io.emit('message', msg);
+    });
+
+});
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server Running On Port ${PORT}`);
 });
