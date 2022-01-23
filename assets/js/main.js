64b5d4863f4f32fc2ed1d8eca6202e206d10c839
tauseedzaman <tauseedzamanofsorana@gmail.com>
2022-01-23 16:37:58 +0500

    my changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,12 @@ let userName;
 let msg;
 let completeMsg;
 let textbox = document.getElementById('text-box');
+// let textbox = document.getElementById('text-box');
 
 document.addEventListener('DOMContentLoaded', function(e){
-    userName = prompt('What is your name?');
-    userName = (userName === '') ? 'Anonymous#'+Math.floor(Math.random() * (100000 - 1 + 1)) : userName;
+    userName = "zaman";
+    // userName = prompt('What is your name?');
+    // userName = (userName === '') ? 'Anonymous#'+Math.floor(Math.random() * (100000 - 1 + 1)) : userName;
 });
 
 textbox.addEventListener('keypress', function(e){
